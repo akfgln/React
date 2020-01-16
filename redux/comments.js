d77f0ac76db3504f47fd1c8f1f7a2e7b66c59829
akfgln <akfgln@gmail.com>
2020-01-16 22:49:08 +0100

    edit

--- redux/comments.js
+++ redux/comments.js
@@ -1,8 +1,12 @@
 import * as ActionTypes from './ActionTypes';
 
 export const comments = (state = { errMess: null, comments:[]}, action) => {
+  debugger;
   switch (action.type) {
     case ActionTypes.ADD_COMMENTS:
+      return {...state, errMess: null, comments: action.payload};
+
+    case ActionTypes.ADD_COMMENT:
       return {...state, errMess: null, comments: action.payload};
 
     case ActionTypes.COMMENTS_FAILED:
