d503824e1cd25b0a6b1afe8aee6563d859ccc8f8
akfgln <akfgln@gmail.com>
2020-01-25 15:45:11 +0100

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Alert, Modal } from 'react-native';
 import DatePicker from 'react-native-datepicker'
 import * as Animatable from 'react-native-animatable';
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
 
 class Reservation extends Component {
 
@@ -38,7 +40,7 @@ class Reservation extends Component {
                                             }, style: 'cancel'},
             {text: 'OK', onPress: () => {
                         console.log(JSON.stringify(this.state));
-                        //this.toggleModal();
+                        this.presentLocalNotification(this.state.date)
                         this.resetForm();
                     }
             },
@@ -56,6 +58,33 @@ class Reservation extends Component {
         });
     }
     
+    async obtainNotificationPermission() {
+        let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        await this.obtainNotificationPermission();
+        Notifications.presentLocalNotificationAsync({
+            title: 'Your Reservation',
+            body: 'Reservation for '+ date + ' requested',
+            ios: {
+                sound: true
+            },
+            android: {
+                sound: true,
+                vibrate: true,
+                color: '#512DA8'
+            }
+        });
+    }
+
     render() {
         return(
             <Animatable.View animation="zoomIn" duration={2000}>   
